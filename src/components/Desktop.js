e2486b2d2f145652dbd91ed148cc306bc293183b
Reza Kohan <persianspc@gmail.com>
2024-10-17 10:34:07 +0330

    Feat: added right click

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import DesktopIcon from './DesktopIcon';
 import Window from './Window';
@@ -27,9 +27,37 @@ const DesktopContainer = styled.div`
   background-repeat: no-repeat;
   position: relative;
 `;
+// Styled components for the context menu
+const ContextMenu = styled.div`
+  position: absolute;
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  padding: 10px;
+  z-index: 1000;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
+`;
+
+const MenuItem = styled.div`
+  padding: 5px 20px;
+  font-size: 13px;
+  cursor: pointer;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #e6e6e6;
+  }
+
+  &.divider {
+    border-top: 1px solid #d1d1d1;
+    margin: 8px 0;
+  }
+`;
 
 function Desktop() {
   const [windows, setWindows] = useState([]);
+  const [contextMenuVisible, setContextMenuVisible] = useState(false);
+  const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
 
   const openWindow = (appName) => {
     if (!windows.find((win) => win.appName === appName)) {
@@ -41,6 +69,25 @@ function Desktop() {
     setWindows(windows.filter((win) => win.appName !== appName));
   };
 
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    setContextMenuPosition({ x: e.pageX, y: e.pageY });
+    setContextMenuVisible(true);
+  };
+
+  const handleClickOutside = () => {
+    setContextMenuVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('contextmenu', handleRightClick);
+    window.addEventListener('click', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('contextmenu', handleRightClick);
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
@@ -101,6 +148,22 @@ function Desktop() {
             {/* <Widgets /> */}
 
       <Dock openWindow={openWindow} />
+
+      <ContextMenu
+        visible={contextMenuVisible}
+        style={{ top: `${contextMenuPosition.y}px`, left: `${contextMenuPosition.x}px` }}
+      >
+        <MenuItem>New Folder</MenuItem>
+        <MenuItem>Get Info</MenuItem>
+        <MenuItem>Change Wallpaper...</MenuItem>
+        <MenuItem className="divider"></MenuItem>
+        <MenuItem>Use Stacks</MenuItem>
+        <MenuItem>Group Stacks By</MenuItem>
+        <MenuItem>Show View Options</MenuItem>
+        <MenuItem className="divider"></MenuItem>
+        <MenuItem>Import from iPhone or iPad</MenuItem>
+      </ContextMenu>
+
     </DesktopContainer>
     </>
   );
